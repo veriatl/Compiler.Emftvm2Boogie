01fae509002ba91066d88f940ea03425a9d9ef67
veriatl <cz0590@gmail.com>
2016-02-12 14:58:00 +0100

    [INIT] init emftvm to boogie project

---
+++ fr.emn.atlanmod.emftvm.compiler/src/ie/nuim/cs/emftvm/core/EMFTVMReader.java
@@ -0,0 +1,13 @@
+package ie.nuim.cs.emftvm.core;
+
+public class EMFTVMReader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
