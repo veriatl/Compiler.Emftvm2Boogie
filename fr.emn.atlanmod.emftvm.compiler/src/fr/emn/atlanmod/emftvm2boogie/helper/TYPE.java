880046940e1b6a9444df282a0ed33f43e610c327
veriatl <cz0590@gmail.com>
2016-02-15 17:07:17 +0100

    init emftvm2boogie project

---
+++ fr.emn.atlanmod.emftvm.compiler/src/fr/emn/atlanmod/emftvm2boogie/helper/TYPE.java
@@ -0,0 +1,16 @@
+package fr.emn.atlanmod.emftvm2boogie.helper;
+
+public enum TYPE {
+	UNKNOWN,
+	ATTR,
+    INT, 
+    BOOL, 
+    STRING, 
+    REF, 
+    SET, 
+    SEQ, 
+    ORDEREDSET, 
+    BAG;
+
+
+}
